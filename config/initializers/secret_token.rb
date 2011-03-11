a4d7ca40651edf633887a243ac3d48f1baeccab1
cedbillard@gmail.com <cbil@MacBook-Pro-de-Cedric-Billard.local>
2011-03-11 21:56:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmsresources::Application.config.secret_token = '21006414081a5e544186f62d3831435960acb290a3ab69f15e1a339f8f9b84e245e5d9485000170172e79002cca32ee8414b4af29ee5c2700a154cd668c54145'
